55905e643deeca9833968b41ac758baf377a4169
David Skravan <navarks@Mac.fritz.box>
2026-01-28 11:25:45 +0100

    Add Xcode project

---
+++ PaddleTrackerWatch/PaddleTrackerWatch Watch App/PaddleTrackerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaddleTrackerWatchApp.swift
+//  PaddleTrackerWatch Watch App
+//
+//  Created by David Skravan on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PaddleTrackerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaddleTrackerWatch/PaddleTrackerWatch Watch AppTests/PaddleTrackerWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaddleTrackerWatch_Watch_AppTests.swift
+//  PaddleTrackerWatch Watch AppTests
+//
+//  Created by David Skravan on 1/28/26.
+//
+
+import Testing
+@testable import PaddleTrackerWatch_Watch_App
+
+struct PaddleTrackerWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
